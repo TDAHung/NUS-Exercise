16d802ed7f3d539980ea71eed4dca5a17fd8b552
TDAHung <tdahung260902@gmail.com>
2023-07-18 15:32:16 +0700

    done follow and like not reload pages

--- Fotobook/app/helpers/discover_photos_helper.rb
+++
@@ -1,2 +0,0 @@
-module DiscoverPhotosHelper
-end
--- Fotobook/test/controllers/like/like_photos_controller_test.rb
+++ Fotobook/test/controllers/like/like_photos_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class DiscoverPhotosControllerTest < ActionDispatch::IntegrationTest
+class Like::LikePhotosControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
